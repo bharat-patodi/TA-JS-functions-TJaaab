3579f4f113bff9652d5621fbfc0459a20eb7fca4
Bharat Patodi <bharatpatodi@gmail.com>
2020-08-18 16:07:29 +0530

    Finish Block 2: Writing your first function

--- block-BJaabj/code/index.js
+++ block-BJaabj/code/index.js
@@ -2,6 +2,10 @@
 1. Create a function named `sayHello` that alerts a message `Hello World!`.
 */
 
+let sayHello = function () {
+  alert("Hello World!");
+};
+
 /*
 2. Create a function named `fullName` with the following steps:
 
@@ -11,6 +15,12 @@
   - Alert `fullName`
 */
 
+function fullName() {
+  let firstName = prompt(`Enter your first name`);
+  let lastName = prompt(`Enter your last name`);
+  let fullName = `${firstName} ${lastName}`;
+  alert(fullName);
+}
 /*
 3. Create a function named `addTwoNumbers` with the following steps:
 
@@ -19,6 +29,13 @@
   - Add the value of firstNum and secondNum and store it in another variable named `sum`.
   - Alert `sum`
 */
+
+function addTwoNumbers() {
+  let firstNum = Number(prompt(`Enter the first number`));
+  let secondNum = Number(prompt(`Enter the second number`));
+  let sum = firstNum + secondNum;
+  alert(sum);
+}
 
 /*
 4. Create a function named `getTable` with the following steps:
@@ -39,12 +56,26 @@
   9 * 10 = 90
 */
 
+function getTable() {
+  let num = Number(prompt(`Enter a number`));
+  for (let i = 1; i <= 10; i++) {
+    console.log(`${num} * ${i} = ${num * i}`);
+  }
+}
+
 /*
 5. Create a function named `isLeapYear` with the following steps:
 
   - Prompts a message saying `Enter a year` and store the value into variable named`year`
   - Alert `[year] is a leap year` otherwise `[year] is not a leap year`
 */
+
+function isLeapYear() {
+  let year = prompt(`Enter a year`);
+  year % 2 == 0
+    ? alert(`${year} is a leap year`)
+    : alert(`${year} is not a leap year`);
+}
 
 /*
 6. Create a function named `getFactorial` with the following steps:
@@ -53,3 +84,32 @@
   - Alert `The factorial of [number] is [factorial]`
 
 */
+
+function getFactorial() {
+  // Ensuring that the number is a natural number
+  let number = Math.round(Number(prompt(`Enter a number`)));
+  let factorial = 1;
+  // Checking for negative numbers
+  if (number < 0) {
+    alert(`Enter a positive number`);
+  }
+  // 0 factorial
+  if (number == 0) {
+    alert(`The factorial of 0 is 1`);
+  } else {
+    // The actual calculation
+    let counter = 1;
+    while (counter <= number) {
+      factorial *= counter;
+      counter++;
+    }
+  }
+  alert(`The factorial of ${number} is ${factorial}`);
+}
+
+// fullName();
+// sayHello();
+// addTwoNumbers();
+// getTable();
+// isLeapYear();
+// getFactorial();
